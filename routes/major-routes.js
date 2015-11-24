25eb2d364f3d6ef56ea4c2b0153ad5d2d84cb7ba
tcontois <tcontois@umass.edu>
2015-11-24 18:00:56 -0500

    major changes

--- routes/major-routes.js
+++ routes/major-routes.js
@@ -6,21 +6,18 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
   var major = req.query.major;
-  var list = '';
+  var list;
+  helper.list( major, (error, courses) => {
+      list = courses || '';
+    });
 	res.render('major', {
-    courses: list, major : major
+    courses: list, major: major
   });
 });
 
 router.post('/select', (req, res) => {
   var major = req.body.major;
-  var list;
-  helper.list( major, (error, courses) => {
-      list = courses || '';
-    });
-
   res.redirect('/major/?major=' + major);
-  
 });
 
 module.exports = router;
