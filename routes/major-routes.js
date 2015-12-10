810f385a2d80d85148fd2eac269f95f0220d90e9
tcontois <tcontois@umass.edu>
2015-12-10 13:10:44 -0500

    more getinspired stuff

--- routes/major-routes.js
+++ routes/major-routes.js
@@ -119,7 +119,113 @@ router.post('/select', (req, res) => {
 });
 
 router.get('/inspire', (req, res) => {
-  res.render('inspire');
+  var user = req.session.user;
+  var major = req.query.major;
+  var concentration = req.query.concentration;
+  var message = req.flash('inspire') || '';
+  // var list;
+  // helper.list( major, (error, courses) => {
+  //   list = courses || '';
+  // });
+  helper.getMajors( (err, majors) =>{
+    if(err){
+      message = err;
+      res.render('inspire', {
+        major: major,
+        concentration: concentration,
+        message: message
+      });
+      return;
+    }
+    if(major){
+      helper.getConcentrations(major, (err, concentrations) => {
+        if(err){
+          message = err;
+          res.render('inspire', {
+            major: major,
+            majors: majors,
+            concentration: concentration,
+            message: message
+          });
+          return;
+        }
+        if(concentration){
+          helper.list(major, concentration, (err, list) => {
+            if(err){
+              message = err;
+              res.render('inspire', {
+                major: major,
+                majors: majors,
+                concentration: concentration,
+                concentrations: concentrations,
+                message: message
+              });
+              return;
+            }
+            if(user){
+              course_helper.listUserCourses(user.name, (error, courses) => {
+                if(error){
+                  message = err;
+                }
+                var info = helper.formatCourseInfo(list, courses);
+                var stats = helper.calculateStats(info);
+                res.render('inspire', {
+                  major: major,
+                  concentration: concentration,
+                  majors: majors,
+                  concentrations: concentrations,
+                  courses: info,
+                  num_completed: stats[0],
+                  num_remaining: stats[1],
+                  message: message
+                });
+              });
+            }
+            else{
+              var courses = helper.formatCourseInfo(list, undefined);
+              res.render('inspire', {
+                major: major,
+                concentration: concentration,
+                majors: majors,
+                concentrations: concentrations,
+                courses: courses,
+                message: message
+              });
+            }
+          });
+        }
+        else{
+          res.render('inspire', {
+            major: major,
+            concentration: concentration,
+            majors: majors,
+            concentrations: concentrations,
+            message: message
+          });
+        }
+      });
+    }
+    else{
+      res.render('inspire', {
+        major: major,
+        concentration: concentration,
+        majors: majors,
+        message: message
+      });
+    }
+  });
+});
+
+router.post('/inspire/concentration', (req, res) => {
+  var major = req.body.major;
+  var concentration = 'GENCOMPSCI';
+  res.redirect('/major/inspire/?form=concentration&major='+major+'&concentration='+concentration);
+});
+
+router.post('/inspire/concentration/reqs', (req, res) => {
+  var major = req.body.major;
+  var concentration = req.body.concentration;
+  res.redirect('/major/?&major='+major+'&concentration='+concentration);
 });
 
 module.exports = router;
